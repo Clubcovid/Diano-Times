f8baf09aa7f9c7e85684dfaaa4fae842686265e1
George Towett <georgedianoh@gmail.com>
2026-02-26 11:55:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ton-safe-mode-87055725-5330a",
+  "appId": "1:748068954779:web:1c4b81b5127c2c4b0adc3e",
+  "apiKey": "AIzaSyBn76repAWoZbF1A2l-KeFiesadQpEV7WU",
+  "authDomain": "ton-safe-mode-87055725-5330a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "748068954779"
+};
